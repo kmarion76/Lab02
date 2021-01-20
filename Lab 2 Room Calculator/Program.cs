0f40afc347307c66297d8e3684b137885164a9af
kmarion76 <47949532+kmarion76@users.noreply.github.com>
2021-01-20 17:16:14 -0500

    Lab02

---
+++ Lab 2 Room Calculator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Lab_2_Room_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double length, width, area, perimeter;
+            string userResponse;
+            Console.WriteLine("Welcome to Grand Circus' Room Detail Generator");
+            Console.WriteLine("");
+            do
+            {
+                Console.Write("Enter Length: ");
+                length = double.Parse(Console.ReadLine());
+
+                Console.Write("Enter Width: ");
+                width = double.Parse(Console.ReadLine());
+
+                area = length * width;
+                perimeter = (2 * length) + (2 * width);
+
+                Console.WriteLine("Area: " + area);
+                Console.WriteLine("Perimeter: " + perimeter);
+
+                Console.WriteLine("");
+
+                Console.Write("Continue? (y/n): ");
+                userResponse = Console.ReadLine();
+
+            } while (userResponse == "y" || userResponse == "Y");
+
+        }
+    }
+}
